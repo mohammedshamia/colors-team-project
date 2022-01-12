90609dc149341f31f818beba51c94812c34e95c1
AhmadSaleh22 <dev.ahmadsaleh@gmail.com>
2022-01-12 17:47:13 +0200

    adding button components

---
+++ src/components/Button/IStyleButton.ts
@@ -0,0 +1,14 @@
+export interface IStyleButton {
+  minWidth?: string;
+  borderRadius?: string;
+  border?: string;
+  fontWeight?: string;
+  background?: string;
+  color?: string;
+  fontSize?: string;
+  cursor?: string;
+  width?: string;
+  height?: string;
+  padding?: string;
+  letterSpace?: string;
+}
