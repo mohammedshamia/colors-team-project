d0625f1afe105f1dd1989869981ee9722313fdde
mohammedali988 <mohd.os.1998@gmail.com>
2022-01-12 16:35:30 +0200

    adding files of divider

---
+++ src/components/Divider/index.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+interface IdividerProps {
+  width?: string;
+  thick?: string;
+  color?: string;
+  margin?: string;
+}
+
+export const Divider = styled('hr')<IdividerProps>(
+  ({
+    width = '100%',
+    thick = '0',
+    color = 'black',
+    margin = '0px',
+    ...props
+  }: IdividerProps) => ({
+    margin: `${margin}`,
+    width: `${width}`,
+    height: `${thick}`,
+    color: `${color}`,
+    borderRadius: '4px',
+  }),
+);
