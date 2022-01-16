677bc25abeb12a6b5dc9b60b2a28b22e8003b910
mohammedali988 <mohd.os.1998@gmail.com>
2022-01-16 18:18:58 +0200

    ubdate

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { Container } from './components';
 
-import Counter from './components/Counter';
-
 const App: React.FC = () => {
   return <Container direction="column" justifyContent="space-between" />;
 };
