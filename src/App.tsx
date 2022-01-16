dac31c8c836364ddf4d9454efbfd9003a08eb64c
mohammedali988 <mohd.os.1998@gmail.com>
2022-01-16 18:31:56 +0200

    ubdate for the counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import { Container } from './components';
+import { Container, Divider } from './components';
+import Counter from './components/Counter';
 
 const App: React.FC = () => {
-  return <Container direction="column" justifyContent="space-between" />;
+  return (
+    <Container direction="column" justifyContent="space-between">
+      <Divider thick="5px" />
+      <Counter />
+    </Container>
+  );
 };
 
 export default App;
